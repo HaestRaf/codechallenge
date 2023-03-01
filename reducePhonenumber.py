c069d24e8a49020df7b03c7af6f324d282d2c561
HaestRaf <124159868+HaestRaf@users.noreply.github.com>
2023-03-01 09:31:32 +0100

    Removed the overdosis of conversions

--- reducePhonenumber.py
+++ reducePhonenumber.py
@@ -3,14 +3,18 @@ def reduce_phoneNumber(phoneNr):
     while phoneNr > 9:
         for number in range(0, len(str(phoneNr)), 2):
             if number+1 < len(str(phoneNr)):
-                total += str(int(str(phoneNr)[number]) + int(str(phoneNr)[number+1]))
+                phone_number = str(phoneNr)
+                digit_1 = int(phone_number[number])
+                digit_2 = int(phone_number[number+1])
+                digit_sum = digit_1 + digit_2
+                total += str(digit_sum)
             else:
-                total += str(int(str(phoneNr)[-1]))
+                phone_number = str(phoneNr)
+                final_digit = int(phone_number[-1])
+                total += str(final_digit)
         phoneNr = int(total)   
         total = ""
     return phoneNr
 
 
 print(reduce_phoneNumber(4783926))
-
-
